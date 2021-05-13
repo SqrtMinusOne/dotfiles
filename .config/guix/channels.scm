02540932bf150d4693731513ee08f13bcb008c1a
SqrtMinusOne <thexcloud@gmail.com>
2021-05-13 20:59:34 +0300

    feat(guix): some notes

---
+++ .config/guix/channels.scm
@@ -0,0 +1,6 @@
+;; [[file:../../Guix.org::*Channels][Channels:1]]
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix"))
+       %default-channels)
+;; Channels:1 ends here
