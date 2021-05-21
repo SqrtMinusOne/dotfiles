349e9c66aa7066a85c23914657473b3ce577df3b
SqrtMinusOne <thexcloud@gmail.com>
2021-05-21 20:42:18 +0300

    feat(console): update for Guix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
-# [[file:../../Shell.org::*Fish][Fish:1]]
+# [[file:../../Console.org::*Fish][Fish:1]]
 starship init fish | source
 # Fish:1 ends here
 
-# [[file:../../Shell.org::*Fish][Fish:2]]
+# [[file:../../Console.org::*Fish][Fish:2]]
 fish_vi_key_bindings
 
 alias v="vim"
@@ -13,14 +13,7 @@ alias q="exit"
 alias c="clear"
 # Fish:2 ends here
 
-# [[file:../../Shell.org::*Fish][Fish:3]]
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/pavel/Programs/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-# Fish:3 ends here
-
-# [[file:../../Shell.org::*Fish][Fish:4]]
+# [[file:../../Console.org::*Fish][Fish:4]]
 if ! test -n "$TMUX"; and ! test -n "$IS_EMACS";
     colorscript random
 end
