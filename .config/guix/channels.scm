87e4cd15797198ce514e305f93e558d867a5caf8
SqrtMinusOne <thexcloud@gmail.com>
2021-05-21 20:39:46 +0300

    feat(guix): move packages to my channel

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,6 +1,10 @@
 ;; [[file:../../Guix.org::*Channels][Channels:1]]
-(cons* (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix"))
-       %default-channels)
+(cons*
+ (channel
+  (name 'channel-q)
+  (url "file:///home/pavel/Code/channel-q"))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix"))
+ %default-channels)
 ;; Channels:1 ends here
