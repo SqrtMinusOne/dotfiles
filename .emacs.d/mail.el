f5e7d44aedeb3c9e0969ad200072ae0fa271ad8d
SqrtMinusOne <thexcloud@gmail.com>
2021-07-10 19:44:11 +0300

    feat(mail): set notmuch searches in Mail.org

--- .emacs.d/mail.el
+++ .emacs.d/mail.el
@@ -28,5 +28,15 @@
   (setq send-mail-function #'sendmail-send-it)
   (add-hook 'notmuch-hello-mode-hook
             (lambda () (display-line-numbers-mode 0)))
+  (setq notmuch-saved-searches
+        '((:name "inbox (main)" :query "tag:inbox AND tag:main")
+          (:name "unread (main)" :query "tag:unread AND tag:main")
+          (:name "sent (main)" :query "tag:sent AND tag:main")
+          (:name "all mail (main)" :query "tag:main")
+          (:name "inbox (progin)" :query "tag:inbox AND tag:progin")
+          (:name "unread (progin)" :query "tag:unread AND tag:progin")
+          (:name "sent (progin)" :query "tag:sent AND tag:progin")
+          (:name "all main (progin)" :query "tag:progin")
+          (:name "drafts" :query "tag:draft")))
   (custom-set-faces
    `(notmuch-wash-cited-text ((t (:foreground ,(doom-color 'yellow)))))))
