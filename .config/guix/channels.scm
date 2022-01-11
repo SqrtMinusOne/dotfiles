d5409891f023ea22d7f7384e0497ef79205fe3f5
SqrtMinusOne <thexcloud@gmail.com>
2022-01-11 08:54:25 +0300

    feat(guix): update channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -20,5 +20,15 @@
     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
     (openpgp-fingerprint
      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+ (channel
+  ;; What can possibly go wrong, huh
+  (name 'guix-gaming-games)
+  (url "https://gitlab.com/guix-gaming-channels/games.git")
+  ;; Enable signature verification:
+  (introduction
+   (make-channel-introduction
+    "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+    (openpgp-fingerprint
+     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
  %default-channels)
 ;; Channels:1 ends here
