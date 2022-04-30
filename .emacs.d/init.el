a33abd485d58bf20be4c3667feaa6897c7f401a4
SqrtMinusOne <thexcloud@gmail.com>
2022-04-30 20:20:16 +0300

    feat(emacs): first version of rdrview & pandoc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t -*-
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -1583,7 +1585,11 @@ Returns (<buffer> . <workspace-index>) or nil."
 
 (use-package php-mode
   :straight t
-  :mode "\\.php\\'")
+  :mode "\\.php\\'"
+  :config
+  (add-hook 'php-mode-hook #'smartparens-mode)
+  (add-hook 'php-mode-hook #'lsp)
+  (my/set-smartparens-indent 'php-mode))
 
 (use-package tex
   :straight auctex
@@ -3495,6 +3501,7 @@ Returns (<buffer> . <workspace-index>) or nil."
    "=" 'dired-narrow
    "-" 'dired-create-empty-file
    "~" 'vterm
+   "M-r" 'wdired-change-to-wdired-mode
    "<left>" 'dired-up-directory
    "<right>" 'dired-find-file
    "M-<return>" 'dired-open-xdg))
@@ -3942,6 +3949,9 @@ Returns (<buffer> . <workspace-index>) or nil."
 (defface elfeed-blogs-entry nil
   "Face for the elfeed entries with tag \"blogs\"")
 
+(defface elfeed-govt-entry nil
+  "Face for the elfeed entries with tag \"blogs\"")
+
 (my/use-doom-colors
  (elfeed-search-tag-face :foreground (doom-color 'yellow))
  (elfeed-videos-entry :foreground (doom-color 'red))
@@ -3949,13 +3959,15 @@ Returns (<buffer> . <workspace-index>) or nil."
  (elfeed-emacs-entry :foreground (doom-color 'magenta))
  (elfeed-music-entry :foreground (doom-color 'green))
  (elfeed-podcasts-entry :foreground (doom-color 'yellow))
- (elfeed-blogs-entry :foreground (doom-color 'orange)))
+ (elfeed-blogs-entry :foreground (doom-color 'orange))
+ (elfeed-govt-entry :foreground (doom-color 'dark-cyan)))
 
 (with-eval-after-load 'elfeed
   (setq elfeed-search-face-alist
-        '((twitter elfeed-twitter-entry)
-          (podcasts elfeed-podcasts-entry)
+        '((podcasts elfeed-podcasts-entry)
           (music elfeed-music-entry)
+          (gov elfeed-govt-entry)
+          (twitter elfeed-twitter-entry)
           (videos elfeed-videos-entry)
           (emacs elfeed-emacs-entry)
           (blogs elfeed-blogs-entry)
@@ -3978,7 +3990,9 @@ Returns (<buffer> . <workspace-index>) or nil."
 
 (use-package elfeed-summary
   :commands (elfeed-summary)
-  :straight t)
+  :straight t
+  :config
+  (setq elfeed-summary-filter-by-title t))
 
 (defun my/elfeed-toggle-score-sort ()
   (interactive)
@@ -4259,6 +4273,229 @@ Returns (<buffer> . <workspace-index>) or nil."
  :keymaps 'eww-mode-map
  "+" 'text-scale-increase
  "-" 'text-scale-decrease)
+
+(defun my/rdrview-get (url callback)
+  (let* ((buffer (generate-new-buffer "rdrview"))
+         (proc (start-process "rdrview" buffer "rdrview"
+                              url "-T" "title,sitename,body"
+                              "-H")))
+    (set-process-sentinel
+     proc
+     (lambda (process _msg)
+       (let ((status (process-status process))
+             (code (process-exit-status process)))
+         (cond ((and (eq status 'exit) (= code 0))
+                (progn
+                  (funcall callback
+                           (with-current-buffer (process-buffer process)
+                             (buffer-string)))
+                  (kill-buffer (process-buffer process))) )
+               ((or (and (eq status 'exit) (> code 0))
+                    (eq status 'signal))
+                (let ((err (with-current-buffer (process-buffer process)
+                             (buffer-string))))
+                  (kill-buffer (process-buffer process))
+                  (user-error "Error in rdrview: %s" err)))))))
+    proc))
+
+(defun my/rdrview-parse (dom-string)
+  (let ((dom (with-temp-buffer
+               (insert dom-string)
+               (libxml-parse-html-region (point-min) (point-max)))))
+    (let (title sitename content)
+      (dolist (child (dom-children (car (dom-by-id dom "readability-page-1"))))
+        (when (listp child)
+          (cond
+           ((eq (car child) 'h1)
+            (setq title (dom-text child)))
+           ((eq (car child) 'h2)
+            (setq sitename (dom-text child)))
+           ((eq (car child) 'div)
+            (setq content child)))))
+      (dom-search
+       content
+       (lambda (el)
+         (when (listp el)
+           (pcase (car el)
+             ('h2 (setf (car el) 'h1))
+             ('h3 (setf (car el) 'h2))
+             ('h4 (setf (car el) 'h3))
+             ('h5 (setf (car el) 'h4))
+             ('h6 (setf (car el) 'h5))))))
+      `((title . ,title)
+        (sitename . ,sitename)
+        (content . ,(with-temp-buffer
+                      (dom-print content)
+                      (buffer-string)))))))
+
+(setq my/rdrview-template (expand-file-name
+                           (concat user-emacs-directory "rdrview.tex")))
+
+(cl-defun my/rdrview-render (content type variables callback
+                                     &key file-name overwrite)
+  (unless file-name
+    (setq file-name (format "/tmp/%d.pdf" (random 100000000))))
+  (let (params
+        (temp-file-name (format "/tmp/%d.%s" (random 100000000) type)))
+    (cl-loop for (key . value) in variables
+             when value
+             do (progn
+                  (push "--variable" params)
+                  (push (format "%s=%s" key value) params)))
+    (setq params (nreverse params))
+    (if (and (file-exists-p file-name) (not overwrite))
+        (funcall callback file-name)
+      (with-temp-file temp-file-name
+        (insert content))
+      (let ((proc (apply #'start-process
+                         "pandoc" (get-buffer-create "*Pandoc*") "pandoc"
+                         temp-file-name "-o" file-name
+                         "--pdf-engine=xelatex" "--template" my/rdrview-template
+                         params)))
+        (set-process-sentinel
+         proc
+         (lambda (process _msg)
+           (let ((status (process-status process))
+                 (code (process-exit-status process)))
+             (cond ((and (eq status 'exit) (= code 0))
+                    (progn
+                      (message "Done!")
+                      (funcall callback file-name)))
+                   ((or (and (eq status 'exit) (> code 0))
+                        (eq status 'signal))
+                    (user-error "Error in pandoc. Check the *Pandoc* buffer"))))))))))
+
+(defun my/get-languages (url)
+  (let ((main-lang "english")
+        (other-lang "russian"))
+    (when (string-match-p (rx ".ru") url)
+      (setq main-lang "russian"
+            other-lang "english"))
+    (list main-lang other-lang)))
+
+(defun my/rdrview-open (url overwrite)
+  (interactive
+   (let ((url (read-from-minibuffer
+               "URL: "
+               (if (bound-and-true-p elfeed-show-entry)
+                   (elfeed-entry-link elfeed-show-entry)))))
+     (when (string-empty-p url)
+       (user-error "URL is empty"))
+     (list url current-prefix-arg)))
+  (my/rdrview-get
+   url
+   (lambda (res)
+     (let ((data (my/rdrview-parse res))
+           (langs (my/get-languages url)))
+       (my/rdrview-render
+        (alist-get 'content data)
+        'html
+        `((title . ,(alist-get 'title data))
+          (subtitle . ,(alist-get 'sitename data))
+          (main-lang . ,(nth 0 langs))
+          (other-lang . ,(nth 1 langs)))
+        (lambda (file-name)
+          (start-process "xdg-open" nil "xdg-open" file-name)))))))
+
+(setq my/elfeed-pdf-dir (expand-file-name "~/.elfeed/pdf/"))
+
+(defun my/elfeed-open-pdf (entry overwrite)
+  (interactive (list elfeed-show-entry current-prefix-arg))
+  (let ((authors (mapcar (lambda (m) (plist-get m :name)) (elfeed-meta entry :authors)))
+        (feed-title (elfeed-feed-title (elfeed-entry-feed entry)))
+        (tags (mapconcat #'symbol-name (elfeed-entry-tags entry) ", "))
+        (date (format-time-string "%a, %e %b %Y" (seconds-to-time (elfeed-entry-date entry))))
+        (content (elfeed-deref (elfeed-entry-content entry)))
+        (file-name (concat my/elfeed-pdf-dir
+                           (elfeed-ref-id (elfeed-entry-content entry))
+                           ".pdf"))
+        (main-language "english")
+        (other-language "russian"))
+    (unless content
+      (user-error "No content!"))
+    (setq subtitle
+          (cond
+           ((seq-empty-p authors) feed-title)
+           ((and (not (seq-empty-p (car authors)))
+                 (string-match-p (regexp-quote (car authors)) feed-title)) feed-title)
+           (t (concat (string-join authors ", ") "\\\\" feed-title))))
+    (when (member 'ru (elfeed-entry-tags entry))
+      (setq main-language "russian")
+      (setq other-language "english"))
+    (my/rdrview-render
+     (if (bound-and-true-p my/elfeed-show-rdrview-html)
+         my/elfeed-show-rdrview-html
+       content)
+     (elfeed-entry-content-type entry)
+     `((title . ,(elfeed-entry-title entry))
+       (subtitle . ,subtitle)
+       (date . ,date)
+       (tags . ,tags)
+       (main-lang . ,main-language)
+       (other-lang . ,other-language))
+     (lambda (file-name)
+       (start-process "xdg-open" nil "xdg-open" file-name))
+     :file-name file-name
+     :overwrite current-prefix-arg)))
+
+(defvar-local my/elfeed-show-rdrview-html nil)
+
+(defun my/rdrview-elfeed-show ()
+  (interactive)
+  (unless elfeed-show-entry
+    (user-error "No elfeed entry in this buffer!"))
+  (my/rdrview-get
+   (elfeed-entry-link elfeed-show-entry)
+   (lambda (result)
+     (let* ((data (my/rdrview-parse result))
+            (inhibit-read-only t)
+            (title (elfeed-entry-title elfeed-show-entry))
+            (date (seconds-to-time (elfeed-entry-date elfeed-show-entry)))
+            (authors (elfeed-meta elfeed-show-entry :authors))
+            (link (elfeed-entry-link elfeed-show-entry))
+            (tags (elfeed-entry-tags elfeed-show-entry))
+            (tagsstr (mapconcat #'symbol-name tags ", "))
+            (nicedate (format-time-string "%a, %e %b %Y %T %Z" date))
+            (content (alist-get 'content data))
+            (feed (elfeed-entry-feed elfeed-show-entry))
+            (feed-title (elfeed-feed-title feed))
+            (base (and feed (elfeed-compute-base (elfeed-feed-url feed)))))
+       (erase-buffer)
+       (insert (format (propertize "Title: %s\n" 'face 'message-header-name)
+                       (propertize title 'face 'message-header-subject)))
+       (when elfeed-show-entry-author
+         (dolist (author authors)
+           (let ((formatted (elfeed--show-format-author author)))
+             (insert
+              (format (propertize "Author: %s\n" 'face 'message-header-name)
+                      (propertize formatted 'face 'message-header-to))))))
+       (insert (format (propertize "Date: %s\n" 'face 'message-header-name)
+                       (propertize nicedate 'face 'message-header-other)))
+       (insert (format (propertize "Feed: %s\n" 'face 'message-header-name)
+                       (propertize feed-title 'face 'message-header-other)))
+       (when tags
+         (insert (format (propertize "Tags: %s\n" 'face 'message-header-name)
+                         (propertize tagsstr 'face 'message-header-other))))
+       (insert (propertize "Link: " 'face 'message-header-name))
+       (elfeed-insert-link link link)
+       (insert "\n")
+       (cl-loop for enclosure in (elfeed-entry-enclosures elfeed-show-entry)
+                do (insert (propertize "Enclosure: " 'face 'message-header-name))
+                do (elfeed-insert-link (car enclosure))
+                do (insert "\n"))
+       (insert "\n")
+       (if content
+           (elfeed-insert-html content base)
+         (insert (propertize "(empty)\n" 'face 'italic)))
+       (setq-local my/elfeed-show-rdrview-html content)
+       (goto-char (point-min))))))
+
+(with-eval-after-load 'elfeed
+  (general-define-key
+   :states '(normal)
+   :keymaps 'elfeed-show-mode-map
+   "gp" #'my/rdrview-elfeed-show
+   "gv" #'my/elfeed-open-pdf))
 
 (use-package erc
   :commands (erc erc-tls)
